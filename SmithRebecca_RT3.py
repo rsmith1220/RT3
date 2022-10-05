33e37d19c2d529a23af18751541027b2f9a98714
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2022-10-04 18:16:18 -0600

    luces y planos

--- SmithRebecca_RT3.py
+++ SmithRebecca_RT3.py
@@ -4,8 +4,8 @@ from figures import *
 from lights import *
 
 
-width = 256
-height = 256
+width = 512
+height = 512
 
 # Materiales
 
@@ -25,14 +25,14 @@ rtx = Raytracer(width, height)
 rtx.envMap = Texture("parkingLot.bmp")
 
 rtx.lights.append( AmbientLight(intensity = 0.1 ))
-rtx.lights.append( DirectionalLight(direction = (-1,-1,-1), intensity = 0.8 ))
+rtx.lights.append(PointLight((0,5,5), constant = 1.0, linear = 0.1, quad = 0.05, color = (1,1,1)))
 
 
-# rtx.scene.append(Plane(position=(0,10,0),normal=(0,0,1), material=marble))
+# rtx.scene.append(Plane(position=(0,5,0),normal=(0,1,0), material=stone))
 rtx.scene.append(Plane(position=(0,-10,-20),normal=(0,1,0), material=earth))
 rtx.scene.append(Plane(position=(-50,0,-20),normal=(1,0,0), material=pinkglass))
 rtx.scene.append(Plane(position=(50,0,0),normal=(1,0,0), material=brick))
-# rtx.scene.append(Plane(position=(0,-10,0),normal=(0,1,0), material=brick))
+# rtx.scene.append(Plane(position=(0,0,1),normal=(1,0,0), material=brick))
 
 rtx.scene.append(AABB(position=(2,0,-8),size=(2,2,2), material=stone))
 rtx.scene.append(AABB(position=(-1,0,-8),size=(2,2,2), material=brick))
