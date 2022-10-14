9a063666b16fd302d0cff068a13452319e820281
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2022-10-13 22:45:04 -0600

    final

--- SmithRebecca_RT3.py
+++ SmithRebecca_RT3.py
@@ -13,7 +13,8 @@ brick = Material(diffuse = (2.55, 1.27, 0.8), spec = 16,matType = OPAQUE)
 stone = Material(diffuse = (0.4, 0.4, 0.4), spec = 8,matType = OPAQUE)
 
 earth = Material(texture = Texture("earthDay.bmp"))
-marble = Material(diffuse = (0.8,0.8,0.8), texture = Texture("marble.bmp"), spec = 32, matType= REFLECTIVE)
+# marble = Material(diffuse = (0.8,0.8,0.8), texture = Texture("marble.bmp"), spec = 32, matType= REFLECTIVE)
+marble = Material(spec = 64, texture = Texture("marble.bmp"), matType= REFLECTIVE)
 
 mirror = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, matType = REFLECTIVE)
 glass = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, ior=1.5, matType = TRANSPARENT)
@@ -28,14 +29,14 @@ rtx.lights.append( AmbientLight(intensity = 0.1 ))
 rtx.lights.append(PointLight((0,5,5), constant = 1.0, linear = 0.1, quad = 0.05, color = (1,1,1)))
 
 
-# rtx.scene.append(Plane(position=(0,5,0),normal=(0,1,0), material=stone))
-rtx.scene.append(Plane(position=(0,-10,-20),normal=(0,1,0), material=earth))
-rtx.scene.append(Plane(position=(-50,0,-20),normal=(1,0,0), material=pinkglass))
-rtx.scene.append(Plane(position=(50,0,0),normal=(1,0,0), material=brick))
-# rtx.scene.append(Plane(position=(0,0,1),normal=(1,0,0), material=brick))
+rtx.scene.append( Plane(position = (0,-10,0), normal = (0,1,0), material = brick ))
+rtx.scene.append( Plane(position = (0,10,0), normal = (0,-1,0), material = brick ))
+rtx.scene.append( Plane(position = (-10,0,0), normal = (1,0,0), material = stone ))
+rtx.scene.append( Plane(position = (10,0,0), normal = (-1,0,0), material = stone ))
+rtx.scene.append( Plane(position = (0,0,-40), normal = (0,0,1), material = stone ))
 
-rtx.scene.append(AABB(position=(2,0,-8),size=(2,2,2), material=stone))
-rtx.scene.append(AABB(position=(-1,0,-8),size=(2,2,2), material=brick))
+rtx.scene.append(AABB(position=(2,0,-8),size=(2,2,2), material=marble))
+rtx.scene.append(AABB(position=(-1,0,-8),size=(2,2,2), material=stone))
 
 
 rtx.glRender()
